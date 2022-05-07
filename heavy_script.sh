e106cab5040f6053b68a188d8a6191f9480f88b1
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-05-07 10:40:50 +0200

    Update heavy_script.sh

--- heavy_script.sh
+++ heavy_script.sh
@@ -2,12 +2,11 @@
 
 dir=$(pwd)
 git remote set-url origin https://github.com/truecharts/truescript.git 2>&1 >/dev/null
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git fetch 2>&1 >/dev/null
 git update-index -q --refresh 2>&1 >/dev/null
 echo "script requires update"
 git reset --hard 2>&1 >/dev/null
-git checkout $BRANCH 2>&1 >/dev/null
+git checkout main 2>&1 >/dev/null
 git pull 2>&1 >/dev/null
 echo "script updated"
 $dir/truescript.sh
