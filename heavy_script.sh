761608ea033e6b140ca7e5e4ba6d5aa69f054336
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-05-07 10:19:22 +0200

    Update heavy_script.sh

--- heavy_script.sh
+++ heavy_script.sh
@@ -5,16 +5,11 @@ git remote set-url origin https://github.com/truecharts/truescript.git 2>&1 >/de
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git fetch 2>&1 >/dev/null
 git update-index -q --refresh 2>&1 >/dev/null
-if [[ `git status --porcelain` ]]; then
-    echo "script requires update"
-    git reset --hard 2>&1 >/dev/null
-    git checkout $BRANCH 2>&1 >/dev/null
-    git pull 2>&1 >/dev/null
-    echo "script updated"
-    
-else
-    echo "script up-to-date"
-fi
+echo "script requires update"
+git reset --hard 2>&1 >/dev/null
+git checkout $BRANCH 2>&1 >/dev/null
+git pull 2>&1 >/dev/null
+echo "script updated"
 chmod +x $dir/heavy_script.sh
 chmod +x $dir/truescript.sh
 $dir/truescript.sh
