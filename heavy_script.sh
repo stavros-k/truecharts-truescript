f92a150076055e04dcd9941e1838008e69f6ad31
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-05-07 10:13:18 +0200

    Update heavy_script.sh

--- heavy_script.sh
+++ heavy_script.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 dir=$(pwd)
-git remote set-url origin https://github.com/truecharts/truescript.git
+git remote set-url origin https://github.com/truecharts/truescript.git 2>&1 >/dev/null
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-git fetch
-git update-index -q --refresh
+git fetch 2>&1 >/dev/null
+git update-index -q --refresh 2>&1 >/dev/null
 CHANGED=$(git diff --name-only origin/$BRANCH)
 if [ ! -z "$CHANGED" ];
 then
     echo "script requires update"
-    git reset --hard
-    git checkout $BRANCH
-    git pull
+    git reset --hard 2>&1 >/dev/null
+    git checkout $BRANCH 2>&1 >/dev/null
+    git pull 2>&1 >/dev/null
     echo "script updated"
     
 else
