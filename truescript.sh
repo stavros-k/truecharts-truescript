c66cae61fd54882d00fa525ae9952338eaa6f777
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-05-08 10:29:27 +0200

    use different update check

--- truescript.sh
+++ truescript.sh
@@ -5,7 +5,7 @@ git remote set-url origin https://github.com/truecharts/truescript.git 2>&1 >/de
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git fetch 2>&1 >/dev/null
 git update-index -q --refresh 2>&1 >/dev/null
-if [[ `git status --porcelain` ]]; then
+if [[ `git status --branch --porcelain` ]]; then
   echo "script requires update"
   git reset --hard 2>&1 >/dev/null
   git checkout $BRANCH 2>&1 >/dev/null
